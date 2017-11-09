9fb60d66b4cbdeb6f1f2b68d94b34a2fbab2a8e9
psxcode <psxcode@gmail.com>
2017-11-09 15:49:25 +0100

    fix(linter): Fix TSLint config

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,9 +1,10 @@
-import {expect} from 'chai';
-import {main} from './'
+import { expect } from 'chai'
+import { main } from './'
 
 describe('main test', function () {
 
   it('should greet you', function () {
     expect(main('Doge')).eq('Hello Doge')
-  });
-});
+  })
+
+})
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export function main(hello: string): string {
-  return `Hello ${hello}`;
+export function main (hello: string): string {
+  return `Hello ${hello}`
 }
