50a8abeffbed7d5d982c51999793509e9b504090
alex.feinstein <alex.feinstein@klarna.com>
2017-11-23 14:49:23 +0100

    chore(Reformat files):

--- rollup.config.js
+++ rollup.config.js
@@ -2,16 +2,16 @@ import typescript from 'rollup-plugin-typescript2';
 import uglify from 'rollup-plugin-uglify';
 
 export default {
-	input: 'src/index.ts',
-	output: {
+  input: 'src/index.ts',
+  output: {
     format: 'cjs',
-	  file: 'dist/index.js'
+    file: 'dist/index.js'
   },
 
-	plugins: [
-	  typescript(),
-	  uglify()
+  plugins: [
+    typescript(),
+    uglify()
   ],
 
-	external: [],
+  external: [],
 }
